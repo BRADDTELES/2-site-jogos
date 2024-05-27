e686902c2e408b5d8e53e2c665eb580e6a676ac9
BRADDTELES <braddteles@gmail.com>
2024-05-27 00:38:14 -0300

    ft. inserindo codigos php no index e diminuindo a imagem da capa no css 'não ficou bom'

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de Jogoss</title>
     <link rel="stylesheet" href="estilos/estilo.css">
 </head>
+
 <body>
+    <?php
+    require_once "includes/banco.php";
+    require_once "includes/funcoes.php";
+    ?>
     <div id="corpo">
         <h1>Escolha seu jogo</h1>
         <table class="listagem">
-            <tr><td>Foto<td>Nome<td>Adm
-            <tr><td>Foto<td>Nome<td>Adm
-            <tr><td>Foto<td>Nome<td>Adm
-            <tr><td>Foto<td>Nome<td>Adm
+            <?php
+            $busca = $banco->query("SELECT * FROM jogos ORDER BY nome");
+            if (!$busca) {
+                echo "<tr><td>Infelizmente a busca deu errado";
+            } else {
+                if ($busca->num_rows == 0) {
+                    echo "<tr><td>Nenhum registro emcontrado";
+                } else {
+                    while ($reg = $busca->fetch_object()) {
+                        $t = thumb($reg->capa);
+                        echo "<tr><td><img src='$t' class='mini'/></td><td>$reg->nome</td><td>Adm</td></tr>";
+                    }
+                }
+            }
+            ?>
         </table>
     </div>
+    <?php $banco->close(); ?>
 </body>
+
 </html>
